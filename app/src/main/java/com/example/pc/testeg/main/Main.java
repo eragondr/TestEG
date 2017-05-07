544f10a39eed3fd6fe2bac12b3865a2d89bff3c4
toan <toannguyen201194@gmail.com>
2017-05-07 15:53:42 +0700

    Random TextView

--- app/src/main/java/com/example/pc/testeg/main/Main.java
+++ app/src/main/java/com/example/pc/testeg/main/Main.java
@@ -2,8 +2,6 @@ package com.example.pc.testeg.main;
 
 import android.graphics.Typeface;
 import android.text.Spannable;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -15,33 +13,21 @@ import java.util.Random;
 
 import static android.R.color.holo_red_dark;
 
-
 public class Main {
-    CustomsText customsText = new CustomsText();
-
-    public   TextView Boss;
-    public  EditText UserInput;
+    public TextView Boss;
+    public EditText UserInput;
     public TextView UsersOutPut;
-    public Button OK;
+    private ArrayList<Vocabulary> table = new ArrayList<Vocabulary>();
+    private ArrayList<String> hasInput = new ArrayList<String>();
+    private String OutPut = "";
+    private String InPut = "";
 
     public Main() {
     }
 
-    public Main(String inPut, ArrayList<Vocabulary> table, ArrayList<String> hasInput, String outPut) {
-
-        InPut = inPut;
+    public Main(ArrayList<Vocabulary> table) {
         this.table = table;
-        this.hasInput = hasInput;
-        OutPut = outPut;
     }
-
-    private ArrayList<Vocabulary> table = new ArrayList<Vocabulary>();
-
-    private ArrayList<String> hasInput = new ArrayList<String>();
-
-    private String OutPut = "";
-
-    private String InPut = "";
 
     public ArrayList<Vocabulary> getTable() {
         return table;
@@ -75,54 +61,45 @@ public class Main {
         InPut = inPut;
     }
 
-
-
     String words = "abcdefghijklmnopqrstuvwxyz";
 
-    public void FirstRandom() {
-        Spannable Item;
-
+    public String FirstRandom() {
         int index = new Random().nextInt(getTable().size() - 1);
-        String item = getTable().get(index).GetItem(new Random().nextInt(getTable().get(index).GetSize()));
+        String item =
+            getTable().get(index).GetItem(new Random().nextInt(getTable().get(index).GetSize()));
         if (getTable().get(index).Remove(item)) {
             getHasInput().add(item);
             setOutPut(item);
         }
-        Item = customsText.CustomsText(Boss.getContext(), item, 0, item.length(), 2f, R.color.colorPrimaryDark, Typeface.BOLD_ITALIC);
-        Boss.setText("Let me start first:" + Item);
-
+        return item;
     }
 
     public void Random() {
         Spannable Item;
         Spannable CustomsLastCharOfOutPut;
-
-
         char LastCharOfInput = getInPut().charAt(getInPut().length() - 1);
-
         int index = words.indexOf(LastCharOfInput);
-
         int IndexOfFirstCharOfInput = words.indexOf(getInPut().charAt(0));
-
         getHasInput().add(getInPut());
         getTable().get(IndexOfFirstCharOfInput).Remove(getInPut());
-
-        String item = getTable().get(index).GetItem(new Random().nextInt(getTable().get(index).GetSize()));
+        String item =
+            getTable().get(index).GetItem(new Random().nextInt(getTable().get(index).GetSize()));
         if (getTable().get(index).Remove(item)) {
             getHasInput().add(item);
             setOutPut(item);
         }
-        Item = customsText.CustomsText(Boss.getContext(), item, 0, item.length()-1, 2f, R.color.colorPrimaryDark, Typeface.ITALIC);
-        CustomsLastCharOfOutPut = customsText.CustomsText(Boss.getContext(), item, item.length()-1, item.length(), 2f, holo_red_dark, Typeface.BOLD_ITALIC);
+        Item = CustomsText.CustomsText(Boss.getContext(), item, 0, item.length() - 1, 2f,
+            R.color.colorPrimaryDark, Typeface.ITALIC);
+        CustomsLastCharOfOutPut = CustomsText
+            .CustomsText(Boss.getContext(), item, item.length() - 1, item.length(), 2f,
+                holo_red_dark, Typeface.BOLD_ITALIC);
         Boss.setText(Item);
         char LastCharOfOutPut = getOutPut().charAt(getOutPut().length() - 1);
         int LastCharOfOutPutIndex = words.indexOf(LastCharOfOutPut);
         int size = getTable().get(LastCharOfOutPutIndex).GetSize();
-
         if (size == 0) {
-
-
-            Boss.setText("The words starting with" + CustomsLastCharOfOutPut + " You can find the words starting with any other letters");
+            Boss.setText("The words starting with" + CustomsLastCharOfOutPut +
+                " You can find the words starting with any other letters");
             Input2(getInPut());
         }
     }
@@ -133,31 +110,36 @@ public class Main {
         Spannable CustomsLasLastCharOfInPut;
         int index = GetRandomArrayWord();
         // char LastCharOfInPut = getInPut().charAt(getInPut().length() - 1);
-        String item = getTable().get(index).GetItem(new Random().nextInt(getTable().get(index).GetSize()));
-        CustomsLastCharOfOutPut = customsText.CustomsText(Boss.getContext(), item, item.length()-1, item.length(), 2f,holo_red_dark, Typeface.BOLD_ITALIC);
-        CustomsLasLastCharOfInPut =customsText.CustomsText(Boss.getContext(),getInPut(), getInPut().length()-1,getInPut().length(), 2f,holo_red_dark, Typeface.BOLD_ITALIC);
-        Item = customsText.CustomsText(Boss.getContext(), item, 0, item.length()-1, 2f, R.color.colorPrimaryDark, Typeface.ITALIC);
-
+        String item =
+            getTable().get(index).GetItem(new Random().nextInt(getTable().get(index).GetSize()));
+        CustomsLastCharOfOutPut = CustomsText
+            .CustomsText(Boss.getContext(), item, item.length() - 1, item.length(), 2f,
+                holo_red_dark, Typeface.BOLD_ITALIC);
+        CustomsLasLastCharOfInPut = CustomsText
+            .CustomsText(Boss.getContext(), getInPut(), getInPut().length() - 1,
+                getInPut().length(), 2f, holo_red_dark, Typeface.BOLD_ITALIC);
+        Item = CustomsText.CustomsText(Boss.getContext(), item, 0, item.length() - 1, 2f,
+            R.color.colorPrimaryDark, Typeface.ITALIC);
         if (getTable().get(index).Remove(item)) {
             getHasInput().add(item);
             setOutPut(item);
         }
-        Boss.setText("The words starting with: " +CustomsLasLastCharOfInPut + " \n .Have been used up. I will find the words starting with any other letters. " + "\n That will be: " + Item);
+        Boss.setText("The words starting with: " + CustomsLasLastCharOfInPut +
+            " \n .Have been used up. I will find the words starting with any other letters. " +
+            "\n That will be: " + Item);
         char LastCharOfOutPut = getOutPut().charAt(getOutPut().length() - 1);
         int LastCharOfOutPutIndex = words.indexOf(LastCharOfOutPut);
         int size = getTable().get(LastCharOfOutPutIndex).GetSize();
         if (size == 0) {
-
-            Boss.setText("The words starting with: " + CustomsLastCharOfOutPut  + " \n.Have been used up. You can find the words starting with any other letters");
+            Boss.setText("The words starting with: " + CustomsLastCharOfOutPut +
+                " \n.Have been used up. You can find the words starting with any other letters");
             Input2(getInPut());
         }
     }
 
     public int GetRandomArrayWord() {
-
         int i = 0;
         for (Vocabulary item : getTable()) {
-
             if (item.GetSize() > 0) {
                 return i;
             }
@@ -167,51 +149,34 @@ public class Main {
     }
 
     public String CheckInput(String input) {
-
+        String inputText = input;
         if (getHasInput().contains(input)) {
-            Boss.setText("You have used this word before, let try a new one");
-            return input;
-
+            return inputText = "You have used this word before, let try a new one";
         }
-
         if (!Contains(input)) {
-            Boss.setText("Opps, I don't recognize this word,let check it again or try a new ơne");
-            return input;
+            return inputText = "Opps, I don't recognize this word,let check it again or try a new" +
+                " ơne";
         }
-
         char FirstCharOfInPut = input.charAt(0);
         //   int wordsIndex = words.indexOf(FirstCharOfInPut);
-
         char LastCharOfOutPut = getOutPut().charAt(getOutPut().length() - 1);
-
         if (FirstCharOfInPut != LastCharOfOutPut) {
-            System.out.println("Please find the new word starts with: " + LastCharOfOutPut);
-
-            return input;
+            return inputText="Please find the new word starts with: " + LastCharOfOutPut;
         }
-
         char LastCharOfInPut = input.charAt(input.length() - 1);
         int LastCharOfInPutIndex = words.indexOf(LastCharOfInPut);
-
         if (getTable().get(LastCharOfInPutIndex).GetSize() == 0) {
             char FirstCharOfinput = input.charAt(0);
             int IndexOfFirstCharOfinput = words.indexOf(FirstCharOfinput);
             getTable().get(IndexOfFirstCharOfinput).Remove(input);
             getHasInput().add(input);
             Random2();
-            return input;
+            return inputText;
         } else {
-
             setInPut(input);
-            UsersOutPut.setText(input);
             Random();
-
+            return inputText;
         }
-
-
-        return input;
-
-
     }
 
     public Boolean Contains(String input) {
@@ -224,36 +189,26 @@ public class Main {
     }
 
     public String Input2(String Input) {
-
         Input = UserInput.getText().toString();
-
         if (!Contains(Input)) {
             System.out.println("Từ bạn nhập không có trong dữ liệu.1");
             return Input;
-
         }
         if (getHasInput().contains(Input)) {
             System.out.println("Tu ban nhap da duoc su dung");
             return Input;
-
         }
         setInPut(Input);
         UsersOutPut.setText(Input);
-
         Random();
-
         return Input;
-
     }
 
-    public void Input() {
-
+   /* public void Input() {
         FirstRandom();
         String Input;
-
         while (true) {
             Input = UserInput.getText().toString();
-
             final String finalInput = Input;
             OK.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -261,12 +216,8 @@ public class Main {
                     CheckInput(finalInput);
                 }
             });
-
-
         }
-
-    }
-
+    }*/
 }
 
 
